65ec534e98f3dc2519c7b1992bcaa44b46fbfd65
Shreyam Pokharel <pshreyam@gmail.com>
2021-01-03 20:14:04 +0545

    ...

--- basic_scripts/python_class.py
+++ basic_scripts/python_class.py
@@ -1,18 +1,22 @@
-#implementation of classes and objects in python
+# implementation of classes and objects in python
+
+
 class Employee:
     pass
-emp_1=Employee()
-emp_2=Employee()
+    
+    
+emp_1 = Employee()
+emp_2 = Employee()
 
-emp_1.first="Hira"
-emp_1.last="Lal"
-emp_1.email="Hira.Lal@company.com"
-emp_1.pay=60000
+emp_1.first = "Hira"
+emp_1.last = "Lal"
+emp_1.email = "Hira.Lal@company.com"
+emp_1.pay = 60000
 
-emp_2.first="Gopi"
-emp_2.last="Chand"
-emp_2.email="Gopi.Chand@company.com"
-emp_2.pay=50000
+emp_2.first = "Gopi"
+emp_2.last = "Chand"
+emp_2.email = "Gopi.Chand@company.com"
+emp_2.pay = 50000
 
     
 print(emp_1.pay)
