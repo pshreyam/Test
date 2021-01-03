c8c2753c4cff542e52d295c19305a2daaec2c167
pshreyam <sp02434318@student.ku.edu.np>
2021-01-03 20:14:04 +0545

    ...

--- basic_scripts/python_count_vowels.py
+++ basic_scripts/python_count_vowels.py
@@ -1,15 +1,14 @@
-def countVowels(string):
+def count_vowels(string):
 	count = 0
 	for ch in string:
 		if ch in "aeiouAEIOU":
-			count +=1 
+			count += 1 
 	return count 
 
-# Main code 
-# function calls
-ans="y"
-while (ans=="y"):
-    string=str(input("Enter a string:"))
-    print ("No. of vowels are {0} in \"{1}\"".format(countVowels(string),string))
-    ans=str(input("Enter:\n1. y for yes\n2. Any Key for exit.\n"))
+# main code 
 
+ans = "y"
+while ans == "y":
+    string=str(input("Enter a string:"))
+    print ("No. of vowels are {0} in \"{1}\"".format(count_vowels(string), string))
+    ans = str(input("Enter:\n1. y for yes\n2. Any Key for exit.\n"))
