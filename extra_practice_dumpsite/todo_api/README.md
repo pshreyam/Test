84ba41f968477e98d0a64917c74547ad709b8c65
pshreyam <sp02434318@student.ku.edu.np>
2021-01-30 21:07:48 +0545

    graphql api

---
+++ extra_practice_dumpsite/todo_api/README.md
@@ -0,0 +1,6 @@
+# GraphQL API with Python, Flask and Ariadne
+
+#### References:
+
+* https://github.com/mr-vagabond/flask-graphql-todo
+* https://www.twilio.com/blog/graphql-api-python-flask-ariadne
---
+++ extra_practice_dumpsite/todo_api/api/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.getcwd()}/todo.db'
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
